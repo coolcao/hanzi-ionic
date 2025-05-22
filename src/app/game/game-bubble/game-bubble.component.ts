adc6770b5f1cbe7767766a9a10b40e6cb04256f9
coolcao <me@coolcao.com>
2025-05-22 22:32:45 +0800

    游戏开始前添加语音播报；现在只添加了人体部位分组的单个汉字语音,后面的分组慢慢补充

--- src/app/game/game-bubble/game-bubble.component.ts
+++ src/app/game/game-bubble/game-bubble.component.ts
@@ -5,6 +5,7 @@ import { Game, Scale } from 'phaser';
 
 import { MainScene } from 'src/app/game/game-bubble/main-scene';
 import { timer } from 'rxjs';
+import { AudioService } from 'src/app/service/audio.service';
 
 @Component({
   selector: 'app-game-bubble',
@@ -13,14 +14,16 @@ import { timer } from 'rxjs';
   styleUrls: ['./game-bubble.component.css'],
 })
 export class GameBubbleComponent implements OnInit, AfterViewInit, OnDestroy {
-
+  private baseAudioPath = 'assets/audios';
   private readonly store = inject(Store);
   private readonly route = inject(ActivatedRoute);
+  private readonly audioService = inject(AudioService);
 
   private game!: Game;
 
   currentScore = 0;
-  gameStatus: 'init' | 'playing' | 'end' = 'init';
+  gameStatus: 'init' | 'playing' | 'end' | 'error' = 'init';
+  errorMsg = '';
 
   groupId = signal('');
   group = computed(() => {
@@ -32,7 +35,6 @@ export class GameBubbleComponent implements OnInit, AfterViewInit, OnDestroy {
   });
 
   darkMode = this.store.darkMode;
-  timerSubscription: any;
 
   constructor() { }
 
@@ -59,12 +61,11 @@ export class GameBubbleComponent implements OnInit, AfterViewInit, OnDestroy {
     if (this.game) {
       this.game.destroy(true);
     }
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe();
-    }
+    this.audioService.stopAll();
   }
 
   startGame() {
+    this.currentScore = 0;
     this.gameStatus = 'init';
     const config: Phaser.Types.Core.GameConfig = {
       mode: Scale.FIT,
@@ -80,19 +81,30 @@ export class GameBubbleComponent implements OnInit, AfterViewInit, OnDestroy {
       autoCenter: Phaser.Scale.CENTER_BOTH,
       roundPixels: true,         // 像素对齐
     };
-    this.timerSubscription = timer(3000).subscribe(() => {
+    this.playAudio().then(() => {
       this.gameStatus = 'playing';
       this.game = new Phaser.Game(config);
       this.game.events.on('gameEnd', (score: number) => {
         this.currentScore = score;
         this.gameStatus = 'end';
       });
+    }).catch(err => {
+      this.gameStatus = 'error';
+      this.errorMsg = '播放音频失败：' + err;
     });
   }
   restartGame() {
     this.game.destroy(true);
     this.initTarget();
     this.startGame();
+  }
+
+  private playAudio() {
+    const preloadTip = this.audioService.preload('tip', `${this.baseAudioPath}/hanzi-game-tip.mp3`);
+    const preloadCharacter = this.audioService.preload('character', `${this.baseAudioPath}/character/${this.target()}.mp3`);
+    return Promise.all([preloadTip, preloadCharacter]).then(() => {
+      return this.audioService.playSequence(['tip', 'character']);
+    });
   }
 
 }
