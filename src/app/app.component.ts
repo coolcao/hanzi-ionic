d1e4f01ef9f4057ce4b2b20525557879cfc039b2
coolcao <me@coolcao.com>
2025-05-28 09:33:26 +0800

    调整状态栏样式,适配移动端状态栏

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 import { AudioService } from 'src/app/service/audio.service';
 import { Store } from './store/store';
@@ -10,7 +13,7 @@ import { Store } from './store/store';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private store = inject(Store);
   private router = inject(Router);
   private audioService = inject(AudioService);
@@ -25,7 +28,9 @@ export class AppComponent {
     effect(() => {
       if (this.darkMode()) {
         document.body.classList.add('dark');
+        this.updateStatusBarColor(this.darkMode());
       } else {
+        this.updateStatusBarColor(this.darkMode());
         document.body.classList.remove('dark');
       }
 
@@ -35,6 +40,13 @@ export class AppComponent {
         document.body.style.overflow = 'auto';
       }
     });
+  }
+
+  ngOnInit(): void {
+    if (this.getPlatform() === 'web') {
+      return;
+    }
+    this.initializeApp();
   }
 
   toggleDarkMode() {
@@ -50,5 +62,33 @@ export class AppComponent {
     this.router.navigateByUrl(this.back()!.path);
   }
 
+  // 判断当前平台
+  getPlatform() {
+    // 使用 Capacitor 的 Platforms 来判断当前运行平台
+    if (Capacitor.isNativePlatform()) {
+      if (Capacitor.getPlatform() === 'android') {
+        return 'android';
+      } else if (Capacitor.getPlatform() === 'ios') {
+        return 'ios';
+      }
+    }
+    return 'web';
+
+  }
+
+  updateStatusBarColor(darkMode: boolean) {
+    StatusBar.setBackgroundColor({
+      color: darkMode ? '#2a363b' : '#ffffff'
+    });
+    StatusBar.setStyle({
+      style: darkMode ? Style.Dark : Style.Light
+    });
+  }
+
+  initializeApp() {
+    StatusBar.setOverlaysWebView({ overlay: false }); // 关键设置
+    // 初始化状态栏颜色
+    this.updateStatusBarColor(this.darkMode());
+  }
 
 }
